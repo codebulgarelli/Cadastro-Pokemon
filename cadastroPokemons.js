6189ec43cb183ff67a9d3ce5043ee08aef5357a5
Jay Bulgarelli <131187458+codebulgarelli@users.noreply.github.com>
2025-01-27 14:59:12 -0300

    Add files via upload

---
+++ cadastroPokemons.js
@@ -0,0 +1,12 @@
+let pokemons = ["Poochyena", "Zigzagoon", "Dragonite", "Dragonite", "Dragonite", "Poochyena", "Wurmple"]
+let timePokemons = [
+["Poochyena", 2, "M", 13]
+["Zigzaggon", 2, "F", 13]
+["Dragonite", 5, "M", 25]
+["Dragonite", 5, "F", 24]
+["Dragonite", 5, "F", 24]
+["Poochyena", 3, "F", 15]
+["Wurmple", 2, "M", 7]
+]
+
+console.log(timePokemons[3][0] + "Castrado com sucesso")
